f978f4cf3bfe9bf3dd2c5120e0773a0998a831f1
Hai Trieu Ngoc <trieungochai.dev@gmail.com>
2022-07-27 23:41:21 +0700

    #1 Setup codebase

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
+const connectDB = require("./db/connect");
 
 // middleware
-app.user(express.json());
+app.use(express.json());
+
+// routes
+
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is listening on ${PORT}`);
-});
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(PORT, () => {
+      console.log(`Server is listening on ${PORT}`);
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+start();
